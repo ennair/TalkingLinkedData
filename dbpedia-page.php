542934921e8611d11398df3aaa81cee3f2aaba24
ennair <ennair@live.nl>
2014-04-14 11:44:11 +0200

    Filtered properties, and changed order
    order: (asc, abstract first)

--- dbpedia-page.php
+++ dbpedia-page.php
@@ -2,59 +2,72 @@
 	$page = $_GET["page"];
 
 	$myquery = "
-	SELECT ?label ?value
+	SELECT DISTINCT ?label ?value
 	WHERE {
    
    	{SELECT ?label ?value 
-      	WHERE {
-         ?value2 ?property <http://dbpedia.org/resource/" . $page . "> .
-         ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
-         ?value2 <http://www.w3.org/2000/01/rdf-schema#label> ?value .
-         FILTER (LANG(?label) = 'en' and LANG(?value) = 'en')
-      	}
-      	}
-            
+    	WHERE {
+	?value2 ?property <http://dbpedia.org/resource/" . $page . "> .
+	?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
+	?value2 <http://www.w3.org/2000/01/rdf-schema#label> ?value .
+	MINUS {?value2 <http://dbpedia.org/ontology/wikiPageRedirects> ?x} .
+	MINUS {?value2 <http://dbpedia.org/ontology/wikiPageDisambiguates> ?x} .
+	MINUS {?value2 <http://dbpedia.org/property/aux> ?x} .
+	MINUS {?value2 <http://dbpedia.org/property/data> ?x} .
+	MINUS {?value2 <http://dbpedia.org/property/sub> ?x} .
+	MINUS {?value2 <http://dbpedia.org/property/resources> ?x} .
+	FILTER (LANG(?label) = 'en' and LANG(?value) = 'en')
+	}
+	}
+        
             UNION
             
             {
-            {SELECT ?label ?value
+            {SELECT (?label AS ?abstract) ?value
             WHERE {
             <http://dbpedia.org/resource/" . $page . "> ?property ?value .
             ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
             ?property <http://www.w3.org/2000/01/rdf-schema#range> <http://www.w3.org/2001/XMLSchema#string> .
             FILTER (LANG(?label) = 'en' and LANG(?value) = 'en')
-      	}
-      	}
+      	    }
+            }
             
             UNION
             
             {
             {SELECT DISTINCT ?label ?value
-            WHERE {
-            <http://dbpedia.org/resource/" . $page . "> ?property ?value2 .
-            ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
-            ?value2 <http://www.w3.org/2000/01/rdf-schema#label> ?value .
-            MINUS {?property <http://www.w3.org/2000/01/rdf-schema#range> <http://www.w3.org/2001/XMLSchema#string>} .
-            FILTER (LANG(?label) = 'en' and LANG(?value) = 'en')
-      	}
-      	}
+	    WHERE {
+	    <http://dbpedia.org/resource/" . $page . "> ?property ?value2 .
+	    ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
+	    ?value2 <http://www.w3.org/2000/01/rdf-schema#label> ?value .
+	    MINUS {?property <http://www.w3.org/2000/01/rdf-schema#range> <http://www.w3.org/2001/XMLSchema#string>} .
+	    FILTER (LANG(?label) = 'en' and LANG(?value) = 'en')
+	    }
+      	    }
             
             UNION
             
             {SELECT ?label ?value
-            WHERE {
-            <http://dbpedia.org/resource/" . $page . "> ?property ?value .
-            ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
-            MINUS {?property <http://www.w3.org/2000/01/rdf-schema#range> <http://www.w3.org/2001/XMLSchema#string>} .
-            MINUS {?value <http://www.w3.org/2000/01/rdf-schema#label> ?x} .
-            FILTER (LANG(?label) = 'en')
-      	}
-      	}
-      	}
-      	}
+	    WHERE {
+	    <http://dbpedia.org/resource/" . $page . "> ?property ?value .
+	    ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
+	    MINUS {?property <http://www.w3.org/2000/01/rdf-schema#range> <http://www.w3.org/2001/XMLSchema#string>} .
+	    MINUS {?value <http://www.w3.org/2000/01/rdf-schema#label> ?x} .
+	    MINUS {?property <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#ObjectProperty>} .
+	    MINUS {?property <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#DatatypeProperty>} .
+	    MINUS {?x <http://dbpedia.org/property/align> ?value} .
+	    MINUS {?x <http://dbpedia.org/property/caption> ?value} .
+	    MINUS {?x <http://dbpedia.org/property/image> ?value} .
+	    MINUS {?x <http://dbpedia.org/property/imageCaption> ?value} .
+	    MINUS {?x <http://dbpedia.org/property/note> ?value} .
+	    FILTER (LANG(?label) = 'en')
+	    }
+      }
+      }
+      }
             
-      	}
-            ORDER BY ?label
+      }
+	ORDER BY ASC(UCASE(str(?label)))
 	";
 
 	$encoded_query = urlencode($myquery);
@@ -75,14 +88,15 @@
 	$html = new DOMDocument();
 	$html->loadXML($query);
 	$i = 1;
-	$d = 1;
+	$d = 2;
 	$string;
 
 	print "\n<option dtmf=\"0\" value=\"Back\">Go back to main menu</option>";
+	print "\n<option dtmf=\"1\" value=\"has abstract," . $page . "\">Abstract</option>";
 	
 	foreach($html->getElementsByTagName('binding') as $section) {  
 		//$->evaluate("/sparql/results//binding[@class='label']")
-		if($i % 2 != 0) {
+		if($i % 2 == 0 and $i > 1) {
 			$sectionName = $section->nodeValue;
 			
 			if(strcmp($sectionName, $string) != 0) {
--- dbpedia-section.php
+++ dbpedia-section.php
@@ -11,18 +11,24 @@
 	$page = $keywords[1];
 
 	$myquery = "
-	SELECT ?label ?value
+	SELECT DISTINCT ?label ?value
 	WHERE {
    
    	{SELECT ?label ?value 
-      	WHERE {
-         ?value2 ?property <http://dbpedia.org/resource/" . $page . "> .
-         ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
-         ?value2 <http://www.w3.org/2000/01/rdf-schema#label> ?value .
-         FILTER (LANG(?label) = 'en' and LANG(?value) = 'en')
-      	}
-      	}
-            
+    	WHERE {
+	?value2 ?property <http://dbpedia.org/resource/" . $page . "> .
+	?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
+	?value2 <http://www.w3.org/2000/01/rdf-schema#label> ?value .
+	MINUS {?value2 <http://dbpedia.org/ontology/wikiPageRedirects> ?x} .
+	MINUS {?value2 <http://dbpedia.org/ontology/wikiPageDisambiguates> ?x} .
+	MINUS {?value2 <http://dbpedia.org/property/aux> ?x} .
+	MINUS {?value2 <http://dbpedia.org/property/data> ?x} .
+	MINUS {?value2 <http://dbpedia.org/property/sub> ?x} .
+	MINUS {?value2 <http://dbpedia.org/property/resources> ?x} .
+	FILTER (LANG(?label) = 'en' and LANG(?value) = 'en')
+	}
+	}
+        
             UNION
             
             {
@@ -32,38 +38,45 @@
             ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
             ?property <http://www.w3.org/2000/01/rdf-schema#range> <http://www.w3.org/2001/XMLSchema#string> .
             FILTER (LANG(?label) = 'en' and LANG(?value) = 'en')
-      	}
-      	}
+      	    }
+            }
             
             UNION
             
             {
             {SELECT DISTINCT ?label ?value
-            WHERE {
-            <http://dbpedia.org/resource/" . $page . "> ?property ?value2 .
-            ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
-            ?value2 <http://www.w3.org/2000/01/rdf-schema#label> ?value .
-            MINUS {?property <http://www.w3.org/2000/01/rdf-schema#range> <http://www.w3.org/2001/XMLSchema#string>} .
-            FILTER (LANG(?label) = 'en' and LANG(?value) = 'en')
-      	}
-      	}
+	    WHERE {
+	    <http://dbpedia.org/resource/" . $page . "> ?property ?value2 .
+	    ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
+	    ?value2 <http://www.w3.org/2000/01/rdf-schema#label> ?value .
+	    MINUS {?property <http://www.w3.org/2000/01/rdf-schema#range> <http://www.w3.org/2001/XMLSchema#string>} .
+	    FILTER (LANG(?label) = 'en' and LANG(?value) = 'en')
+	    }
+      	    }
             
             UNION
             
             {SELECT ?label ?value
-            WHERE {
-            <http://dbpedia.org/resource/" . $page . "> ?property ?value .
-            ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
-            MINUS {?property <http://www.w3.org/2000/01/rdf-schema#range> <http://www.w3.org/2001/XMLSchema#string>} .
-            MINUS {?value <http://www.w3.org/2000/01/rdf-schema#label> ?x} .
-            FILTER (LANG(?label) = 'en')
-      	}
-      	}
-      	}
-      	}
+	    WHERE {
+	    <http://dbpedia.org/resource/" . $page . "> ?property ?value .
+	    ?property <http://www.w3.org/2000/01/rdf-schema#label> ?label .
+	    MINUS {?property <http://www.w3.org/2000/01/rdf-schema#range> <http://www.w3.org/2001/XMLSchema#string>} .
+	    MINUS {?value <http://www.w3.org/2000/01/rdf-schema#label> ?x} .
+	    MINUS {?property <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#ObjectProperty>} .
+	    MINUS {?property <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#DatatypeProperty>} .
+	    MINUS {?x <http://dbpedia.org/property/align> ?value} .
+	    MINUS {?x <http://dbpedia.org/property/caption> ?value} .
+	    MINUS {?x <http://dbpedia.org/property/image> ?value} .
+	    MINUS {?x <http://dbpedia.org/property/imageCaption> ?value} .
+	    MINUS {?x <http://dbpedia.org/property/note> ?value} .
+	    FILTER (LANG(?label) = 'en')
+	    }
+      }
+      }
+      }
             
-      	}
-        ORDER BY ASC(?label);
+      }
+	ORDER BY ASC(UCASE(str(?label)))
 	";
 
 	$encoded_query = urlencode($myquery);
